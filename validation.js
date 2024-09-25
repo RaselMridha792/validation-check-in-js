ec0691605fd81d7779b3e8b7f632b4c93a470b21
rasel mridha <raselmridha792@gmail.com>
2024-09-25 12:47:08 +0600

    checking array

--- validation.js
+++ validation.js
@@ -6,3 +6,12 @@ if(typeof x === 'number'){
 else{
     console.log('its not a number');
 }
+
+// checking array 
+const numbers = [33, 44, 22, 456, 2];
+if(Array.isArray(numbers)){
+    console.log('yes, this is an array');
+}
+else{
+    console.log('its not an array');
+}
